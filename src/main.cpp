8ce101023205158e7a88c4ee3fcd42369194c18c
吴凯林 <1264102719@qq.com>
2023-05-19 08:31:52 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,49 @@
 #include <WiFi.h>
 #include <time.h>
+#include <string>
 
-const char *ssid = "云镝智网";         // The SSID (name) of the Wi-Fi network you want to connect to
-const char *password = "yundizhiwang"; // The password of the Wi-Fi network
+// const char *ssid = "esp";           // The SSID (name) of the Wi-Fi network you want to connect to
+// const char *password = "asdfghjkl"; // The password of the Wi-Fi network
+std::string IP;
+
+// 函数申明
+void syncTime();
+
+std::string getTime();
+
+char *strToChar(std::string str);
+
+void connectWifi(char *ssid, char *password);
 
 void setup()
 {
-
   Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+  pinMode(15, OUTPUT);
+  digitalWrite(15, HIGH);
 
+  connectWifi("esp", "asdfghjkl");
+
+  syncTime();
+}
+
+void loop()
+{
+  // 输出事件到串口
+  Serial.println(strToChar(getTime()));
+
+  delay(1000);
+}
+
+/**
+ * @brief 连接wifi
+ *
+ * @param ssid
+ * @param password
+ */
+void connectWifi(char *ssid, char *password)
+{
   WiFi.begin(ssid, password); // Connect to the network
   Serial.print("Connecting to ");
   Serial.print(ssid);
@@ -19,6 +54,19 @@ void setup()
     Serial.print('.');
   }
 
+  Serial.println();
+  Serial.println("WiFi connected.");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  IP = WiFi.localIP().toString().c_str();
+}
+
+/**
+ * @brief 同步时间
+ *
+ */
+void syncTime()
+{
   configTime(8 * 3600, 0, "ntp1.aliyun.com"); // 配置时间服务器
 
   while (!time(nullptr))
@@ -28,7 +76,12 @@ void setup()
   }
 }
 
-void loop()
+/**
+ * @brief 获取事件字符串
+ *
+ * @return std::string
+ */
+std::string getTime()
 {
   time_t now = time(nullptr);
   struct tm timeinfo;
@@ -37,8 +90,18 @@ void loop()
 
   char timeString[20];
   strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M:%S", &timeinfo);
+  return std::string(timeString);
+}
 
-  Serial.println(timeString);
-
-  delay(1000);
+/**
+ * @brief string 转 char*
+ *
+ * @param str
+ * @return char*
+ */
+char *strToChar(std::string str)
+{
+  char *cstr = new char[str.length() + 1];
+  strcpy(cstr, str.c_str());
+  return cstr;
 }
