6a4e748404a330ce0c420434e09a9a67f9506f39
吴凯林 <1264102719@qq.com>
2023-05-19 15:47:26 +0800

    1.新增wifi的扫描连接

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <WiFi.h>
 #include <time.h>
 #include <string>
+#include <list>
 
-// const char *ssid = "esp";           // The SSID (name) of the Wi-Fi network you want to connect to
-// const char *password = "asdfghjkl"; // The password of the Wi-Fi network
+// wifi信息结构体
+struct WifiCredentials
+{
+  std::string ssid;
+  std::string password;
+};
+
 std::string IP;
+// 已知wifi的ssid和password
+const std::list<WifiCredentials> myWifi = {
+    {"esp", "asdfghjkl"},
+    {"云镝智网", "yundizhiwang"}};
 
 // 函数申明
 void syncTime();
@@ -13,8 +23,14 @@ std::string getTime();
 
 char *strToChar(std::string str);
 
-void connectWifi(char *ssid, char *password);
+void scanAndConnectWifi();
 
+void connectWifi(const char *ssid, const char *password);
+
+/**
+ * @brief 初始化
+ *
+ */
 void setup()
 {
   Serial.begin(9600);
@@ -23,11 +39,15 @@ void setup()
   pinMode(15, OUTPUT);
   digitalWrite(15, HIGH);
 
-  connectWifi("esp", "asdfghjkl");
+  scanAndConnectWifi();
 
   syncTime();
 }
 
+/**
+ * @brief 主循环
+ *
+ */
 void loop()
 {
   // 输出事件到串口
@@ -37,12 +57,45 @@ void loop()
 }
 
 /**
+ * @brief 扫描wifi并连接
+ *
+ */
+void scanAndConnectWifi()
+{
+  WiFi.mode(WIFI_STA);
+  Serial.println("Scanning WiFi networks...");
+  int numNetworks = WiFi.scanNetworks();
+  Serial.print("Found ");
+  Serial.print(numNetworks);
+  Serial.println(" networks");
+
+  for (int i = 0; i < numNetworks; i++)
+  {
+    String ssid = WiFi.SSID(i);
+    Serial.print("Found network: ");
+    Serial.println(ssid);
+
+    for (const WifiCredentials &wifi : myWifi)
+    {
+      if (std::string(ssid.c_str()) == wifi.ssid)
+      {
+        Serial.println("Match found! Connecting to WiFi...");
+        connectWifi(wifi.ssid.c_str(), wifi.password.c_str());
+        return;
+      }
+    }
+  }
+
+  Serial.println("No matching WiFi found");
+}
+
+/**
  * @brief 连接wifi
  *
  * @param ssid
  * @param password
  */
-void connectWifi(char *ssid, char *password)
+void connectWifi(const char *ssid, const char *password)
 {
   WiFi.begin(ssid, password); // Connect to the network
   Serial.print("Connecting to ");
